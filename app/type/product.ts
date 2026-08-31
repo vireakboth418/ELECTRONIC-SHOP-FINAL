99d757f1231e56d795f68d6a7f6a504e01953201
Moung Seyha <seyhamoung825@gmail.com>
2026-08-31 10:42:33 +0700

    mergee both

--- app/type/product.ts
+++ app/type/product.ts
@@ -1,9 +1,14 @@
-export type Product ={
-    id: string | number,
-    name: string,
-    price: number,
-    description: string,
-    image: string,
-    category?: string,
-    brand?: string
+export interface Product {
+  id: string
+  name: string
+  category: string
+  price: number
+  discount: number
+  finalPrice: number
+  tag: string
+  image: string
+  description: string
+  brand: string
+  stock: number
+  rating: number
 }
